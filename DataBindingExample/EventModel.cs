b718571229ed754b34ee641638a559a3d0e584b8
felipe-cutz <19470219@itcampeche.edu.mx>
2020-11-05 00:09:35 -0600

    Version utilizando Data Binding a un objeto de datos simple.

---
+++ DataBindingExample/EventModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataBindingExample
+{
+    class EventModel : INotifyPropertyChanged
+    {
+        private string title;
+        private DateTime date;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (value != title)
+                {
+                    title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                if (value != date)
+                {
+                    date = value;
+                    OnPropertyChanged("Date");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+
+    }
+}
+   
+
---
+++ DataBindingExample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingExample
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EventModel evento;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            evento = new EventModel()
+            {
+                Title = "Reunión de trabajo",
+                Date = new DateTime(2020, 10, 23)
+            };
+            NombreTextBox.Text = evento.Title;
+            FechaDatePicker.SelectedDate = evento.Date;
+        }
+
+        private void modificadoreventobutton_Click(object sender, RoutedEventArgs e)
+        {
+            evento.Title = evento.Title.ToLower(); 
+            evento.Date = evento.Date.AddDays(1); 
+            NombreTextBox.Text = evento.Title; 
+            FechaDatePicker.SelectedDate = evento.Date;
+            MessageBox.Show(evento.Title + "\n" + evento.Date);
+        }
+    }
+}
